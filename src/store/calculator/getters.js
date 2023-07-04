21c86427bc650acdafed82bb04fee755f292f574
Alive.Kuo <alegnadise@gmail.com>
2023-07-04 23:46:29 +0800

    fix: incorrectly put skill damage with other damage

--- src/store/calculator/getters.js
+++ src/store/calculator/getters.js
@@ -8,15 +8,16 @@ const theGetters = {
         100 + Number(state.parameters.extraDamage) + Number(state.parameters.bossExtraDamage) +
           (state.equipments.specialAF1 ? 40 : 0) +
           (state.equipments.specialAF2 ? 20 : 0) +
-          (state.equipments.specialGW2 ? 25 : 0)
+          (state.equipments.specialGW2 ? 25 : 0) +
+          Number(state.parameters.skillDamage) +
+          Number(state.parameters.singleSkillDamage)
       ) / 100 *
       (100 + Number(state.parameters.elementBonus)) / 100 *
       (
         100 + Number(state.parameters.criticalDamage) +
           Number(state.parameters.bossCriticalDamage) +
           (state.equipments.specialGW1 ? 25 : 0)
-      ) / 100 *
-      (100 + Number(state.parameters.skillDamage) + Number(state.parameters.singleSkillDamage)) / 100
+      ) / 100
     );
   },
   urldata(state) {
